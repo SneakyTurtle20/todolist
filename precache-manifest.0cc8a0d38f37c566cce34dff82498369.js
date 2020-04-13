fd50dcc74be3fd29c573c27f3948efe2f8a0d53f
Walid Jamarin <walidjamarin21@gmail.com>
2020-04-13 10:58:27 +0800

    fix routes for gh-pages

---
+++ precache-manifest.0cc8a0d38f37c566cce34dff82498369.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "568883a5c1b230bf0977503f05651277",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3bdb0081b10d33f2679a",
+    "url": "/static/js/2.186f7803.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.186f7803.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fedf6dc3e0f4b0b8e034",
+    "url": "/static/js/main.32e2f6c9.chunk.js"
+  },
+  {
+    "revision": "aa5e3f25256ac04104da",
+    "url": "/static/js/runtime-main.94eca264.js"
+  }
+]);
---
+++ precache-manifest.e6439961f095dcf4136e6e6b889c30ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "139d51302b9c4be6e77eea4e05440816",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "3bdb0081b10d33f2679a",
+    "url": "/todolist/static/js/2.186f7803.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todolist/static/js/2.186f7803.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff7642048bb037bbbaaf",
+    "url": "/todolist/static/js/main.8b1d5a1f.chunk.js"
+  },
+  {
+    "revision": "962ca10136ac2a5368bb",
+    "url": "/todolist/static/js/runtime-main.14a15a60.js"
+  }
+]);
